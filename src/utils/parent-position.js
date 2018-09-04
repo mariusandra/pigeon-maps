6f929f7fd06db12c50f460ec4617759a557ddfb6
Marius Andra <marius.andra@gmail.com>
2018-09-04 15:18:43 +0200

    fixes position of element for scroll in position:fixed with the page having scrolled

--- src/utils/parent-position.js
+++ src/utils/parent-position.js
@@ -1,14 +1,4 @@
 export default function parentPosition (element) {
-  var x = -window.scrollX
-  var y = -window.scrollY
-  var first = true
-
-  while (element) {
-    x += (element.offsetLeft - (first ? 0 : element.scrollLeft) + element.clientLeft)
-    y += (element.offsetTop - (first ? 0 : element.scrollTop) + element.clientTop)
-    element = element.offsetParent
-    first = false
-  }
-
-  return { x, y }
+  const rect = element.getBoundingClientRect()
+  return { x: rect.left, y: rect.top }
 }
