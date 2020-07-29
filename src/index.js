9f6c3d562475c2421ab2949a43cf5f8611554db2
maifeeulasad <maifeeulasad@gmail.com>
2020-07-29 14:50:51 +0600

    #105 - included 3 providers from demo, by default stamenToner is being used easy to use, who just want to use OSM

--- src/index.js
+++ src/index.js
@@ -17,13 +17,17 @@ const WARNING_DISPLAY_TIMEOUT = 300
 
 const NOOP = () => {}
 
-function osm (x, y, z) {
-  const s = String.fromCharCode(97 + (x + y + z) % 3)
-  return `https://${s}.tile.openstreetmap.org/${z}/${x}/${y}.png`
-}
-
-function stamenToner (x, y, z, dpr) {
-  return `https://stamen-tiles.a.ssl.fastly.net/toner/${z}/${x}/${y}${dpr >= 2 ? '@2x' : ''}.png`
+export const providers = {
+  osm: (x, y, z) => {
+    const s = String.fromCharCode(97 + (x + y + z) % 3)
+    return `https://${s}.tile.openstreetmap.org/${z}/${x}/${y}.png`
+  },
+  stamenTerrain: (x, y, z, dpr) => {
+    return `https://stamen-tiles.a.ssl.fastly.net/terrain/${z}/${x}/${y}${dpr >= 2 ? '@2x' : ''}.jpg`
+  },
+  stamenToner: (x, y, z, dpr) => {
+    return `https://stamen-tiles.a.ssl.fastly.net/toner/${z}/${x}/${y}${dpr >= 2 ? '@2x' : ''}.png`
+  }
 }
 
 // https://wiki.openstreetmap.org/wiki/Slippy_map_tilenames
@@ -1033,7 +1037,7 @@ export default class Map extends Component {
   renderTiles () {
     const { oldTiles } = this.state
     const { dprs } = this.props
-    const mapUrl = this.props.provider || stamenToner
+    const mapUrl = this.props.provider || providers.stamenToner
 
     const {
       tileMinX,
