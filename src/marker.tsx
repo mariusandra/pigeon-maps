b5f341f4bb00ed47149af0b45fc820222e382e46
Marius Andra <marius.andra@gmail.com>
2020-12-03 22:29:41 +0100

    add color support

--- src/marker.tsx
+++ src/marker.tsx
@@ -35,6 +35,7 @@ export function Marker(props: MarkerProps) {
       : 34
   const [internalHover, setInternalHover] = useState(props.hover || false)
   const hover = typeof props.hover === 'undefined' ? internalHover : props.hover
+  const color = props.color || '#93C0D0'
 
   // what do you expect to get back with the event
   const eventParameters = (event: React.MouseEvent<SVGElement>) => ({
@@ -70,11 +71,11 @@ export function Marker(props: MarkerProps) {
         >
           <path
             d="M52 31.5C52 36.8395 49.18 42.314 45.0107 47.6094C40.8672 52.872 35.619 57.678 31.1763 61.6922C30.7916 62.0398 30.2084 62.0398 29.8237 61.6922C25.381 57.678 20.1328 52.872 15.9893 47.6094C11.82 42.314 9 36.8395 9 31.5C9 18.5709 18.6801 9 30.5 9C42.3199 9 52 18.5709 52 31.5Z"
-            fill="#93C0D0"
+            fill={color}
             stroke="white"
             strokeWidth="4"
           />
-          <circle cx="30.5" cy="30.5" r="8.5" fill={hover ? 'white' : '#CFECF4'} />
+          <circle cx="30.5" cy="30.5" r="8.5" fill="white" opacity={hover ? 0.98 : 0.6} />
         </g>
       </svg>
     </div>
