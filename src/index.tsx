1251e6376cc2f2056c14f5b8c9ba58ff7fde7d5d
Marius Andra <marius.andra@gmail.com>
2020-12-03 22:47:49 +0100

    add Overlay

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 export * from './types'
 export { Map } from './map'
 export { Marker } from './marker'
+export { Overlay } from './overlay'
 
 import { Map } from './map'
 export default Map
---
+++ src/overlay.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { PigeonProps } from './types'
+
+interface OverlayProps extends PigeonProps {
+  style?: React.CSSProperties
+  className?: string
+  children?: React.ReactNode
+}
+
+export function Overlay(props: OverlayProps) {
+  return (
+    <div
+      style={{
+        position: 'absolute',
+        transform: `translate(${props.left}px, ${props.top}px)`,
+        ...(props.style || {}),
+      }}
+      className={props.className ? `${props.className} pigeon-click-block` : 'pigeon-click-block'}
+    >
+      {props.children}
+    </div>
+  )
+}
