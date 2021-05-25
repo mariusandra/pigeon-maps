803a9c884b99a2ee44d6b3473fb44920e2c62806
Kasper Bruus Frank <kbf@scadaminds.com>
2021-05-25 11:15:35 +0200

    set default DPR to 1

--- src/providers.ts
+++ src/providers.ts
@@ -3,14 +3,14 @@ export function osm(x: number, y: number, z: number): string {
   return `https://${s}.tile.openstreetmap.org/${z}/${x}/${y}.png`
 }
 
-export function stamenToner(x: number, y: number, z: number, dpr: number): string {
+export function stamenToner(x: number, y: number, z: number, dpr = 1): string {
   return `https://stamen-tiles.a.ssl.fastly.net/toner/${z}/${x}/${y}${dpr >= 2 ? '@2x' : ''}.png`
 }
 
-export function stamenTerrain(x: number, y: number, z: number, dpr: number): string {
+export function stamenTerrain(x: number, y: number, z: number, dpr = 1): string {
   return `https://stamen-tiles.a.ssl.fastly.net/terrain/${z}/${x}/${y}${dpr >= 2 ? '@2x' : ''}.jpg`
 }
 
-export const maptiler = (apiKey: string, map = 'streets') => (x: number, y: number, z: number, dpr: number): string => {
+export const maptiler = (apiKey: string, map = 'streets') => (x: number, y: number, z: number, dpr = 1): string => {
   return `https://api.maptiler.com/maps/${map}/256/${z}/${x}/${y}${dpr >= 2 ? '@2x' : ''}.png?key=${apiKey}`
 }
