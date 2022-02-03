4675ec8e36e38107ea58b3e76a5e0f40763c3b6e
baldulin <baldulin@systemli.org>
2022-02-03 18:57:41 +0100

    misc: Implement other GeoJson geometries

--- src/overlays/GeoJson.tsx
+++ src/overlays/GeoJson.tsx
@@ -22,6 +22,8 @@ interface GeoJsonProps extends PigeonProps {
   onMouseOut?: ({ event: HTMLMouseEvent, anchor: Point, payload: any }) => void
 }
 
+const defaultSvgAttributes = {fill:"#93c0d099", strokeWidth:"2", stroke:"white", r: "30"};
+
 export function GeoJsonLoader(props: GeoJsonProps): JSX.Element {
   const [data, setData] = useState(null);
 
@@ -34,9 +36,39 @@ export function GeoJsonLoader(props: GeoJsonProps): JSX.Element {
   return data ? <GeoJson data={data} {...props}/> : null;
 };
 
+export function Point(props: GeoJsonProps): JSX.Element {
+  const {latLngToPixel} = props;
+  const [y, x] = props.coordinates;
+  const [cx, cy] = latLngToPixel([x, y]);
+  return <circle cx={cx} cy={cy} {...props.svgAttributes}/>
+}
+
+export function MultiPoint(props: GeoJsonProps): JSX.Element {
+  return props.coordinates.map((point, i) => (
+    <Point {...props} coordinates={point} key={i}/>
+  ))
+}
+
+export function LineString(props: GeoJsonProps): JSX.Element {
+  const {latLngToPixel} = props;
+  const p = "M" + props.coordinates.reduce((a, [y, x]) => {
+    const [v, w] = latLngToPixel([x,y]);
+    return a + " " + v + " " + w; 
+  }, "");
+
+  return <path d={p} {...props.svgAttributes}/>
+}
+
+export function MultiLineString(props: GeoJsonProps): JSX.Element {
+  return props.coordinates.map((line, i) => (
+    <LineString {...props} coordinates={line} key={i}/>
+  ))
+}
+
 export function Polygon(props: GeoJsonProps): JSX.Element {
   const {latLngToPixel} = props;
-  const p = props.polygon.reduce(
+  // GeoJson polygons is a collection of linear rings
+  const p = props.coordinates.reduce(
     (a, part) => (a + " M" + part.reduce((a, [y, x]) => {
             const [v, w] = latLngToPixel([x,y]);
             return a + " " + v + " " + w; 
@@ -45,10 +77,45 @@ export function Polygon(props: GeoJsonProps): JSX.Element {
   return <path d={p} {...props.svgAttributes}/>
 }
 
+export function MultiPolygon(props: GeoJsonProps): JSX.Element {
+  return props.coordinates.map((polygon, i) => (
+    <Polygon {...props} coordinates={polygon} key={i}/>
+  ))
+}
+
+export function GeometryCollection(props: GeoJsonProps): JSX.Element {
+  const renderer = {
+    Point, MultiPoint, LineString, MultiLineString, Polygon, MultiPolygon
+  };
+
+  const {type, coordinates, geometries} = props.geometry;
+  
+  if(type === "GeometryCollection"){
+    return geometries.map((geometry) => (
+        <GeometryCollection {...props} geometry={geometry}/>
+    ));
+  }
+
+  const Component = renderer[type];
+
+  if(Component === undefined){
+     console.warn(`The GeoJson Type ${type} is not known`);
+     return null;
+  }
+  return <Component {...props} coordinates={coordinates} svgAttributes={props.svgAttributes}/>
+}
+
 export function GeoJsonFeature(props: GeoJsonProps): JSX.Element {
   const [internalHover, setInternalHover] = useState(props.hover || false)
   const hover = props.hover || internalHover;
   const callbackSvgAttributes = props.styleCallback && props.styleCallback(props.feature, hover);
+  const svgAttributes = callbackSvgAttributes 
+    ? props.svgAttributes
+        ? {...props.svgAttributes, ...callbackSvgAttributes}
+        : callbackSvgAttributes
+    : props.svgAttributes
+        ? props.svgAttributes
+        : defaultSvgAttributes;
 
   // what do you expect to get back with the event
   const eventParameters = (event: React.MouseEvent<SVGElement>) => ({
@@ -57,28 +124,7 @@ export function GeoJsonFeature(props: GeoJsonProps): JSX.Element {
     payload: props.feature,
   })
 
-  let children = null;
-  if(props.geometry.type === "Polygon"){
-    children = <Polygon
-        {...props}
-        polygon={props.geometry.coordinates}
-        hover={props.hover || internalHover}
-        svgAttributes={callbackSvgAttributes ? {...props.svgAttributes, ...callbackSvgAttributes} : props.svgAttributes}
-    />
-  }
-  else if(props.geometry.type === "MultiPolygon"){
-    children = props.geometry.coordinates.map(
-        (polygon, i) => <Polygon
-            key={i}
-            {...props}
-            polygon={polygon}
-            hover={props.hover || internalHover}
-            svgAttributes={callbackSvgAttributes ? {...props.svgAttributes, ...callbackSvgAttributes} : props.svgAttributes}
-            />
-    );
-  }
-
-    return <g
+  return <g
           clipRule="evenodd"
           style={{ pointerEvents: 'auto' }}
           onClick={props.onClick ? (event) => props.onClick(eventParameters(event)) : null}
@@ -92,7 +138,7 @@ export function GeoJsonFeature(props: GeoJsonProps): JSX.Element {
             setInternalHover(false)
           }}
     >
-        {children}
+        <GeometryCollection {...props} svgAttributes={svgAttributes}/>
     </g>
 };
 
@@ -103,6 +149,8 @@ export function GeoJson(props: GeoJsonProps): JSX.Element {
     <div
       style={{
         position: 'absolute',
+        left: '0',
+        top: '0',
         pointerEvents: 'none',
         cursor: 'pointer',
         ...(props.style || {}),
