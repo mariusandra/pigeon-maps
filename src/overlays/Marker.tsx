84f548270e4e839b2bdd578d009a42411769fc6e
Said Adibilarijani <said.adibilarijani@syzygy.de>
2022-07-04 11:24:07 +0200

    Add basic custom marker support

--- src/overlays/Marker.tsx
+++ src/overlays/Marker.tsx
@@ -13,6 +13,8 @@ interface MarkerProps extends PigeonProps {
   style?: React.CSSProperties
   className?: string
 
+  children?: JSX.Element
+
   // callbacks
   onClick?: ({ event: HTMLMouseEvent, anchor: Point, payload: any }) => void
   onContextMenu?: ({ event: HTMLMouseEvent, anchor: Point, payload: any }) => void
@@ -55,30 +57,30 @@ export function Marker(props: MarkerProps): JSX.Element {
         ...(props.style || {}),
       }}
       className={props.className ? `${props.className} pigeon-click-block` : 'pigeon-click-block'}
+      onClick={props.onClick ? (event) => props.onClick(eventParameters(event)) : null}
+      onContextMenu={props.onContextMenu ? (event) => props.onContextMenu(eventParameters(event)) : null}
+      onMouseOver={(event) => {
+        props.onMouseOver && props.onMouseOver(eventParameters(event))
+        setInternalHover(true)
+      }}
+      onMouseOut={(event) => {
+        props.onMouseOut && props.onMouseOut(eventParameters(event))
+        setInternalHover(false)
+      }}
     >
-      <svg width={width} height={height} viewBox="0 0 61 71" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <g
-          style={{ pointerEvents: 'auto' }}
-          onClick={props.onClick ? (event) => props.onClick(eventParameters(event)) : null}
-          onContextMenu={props.onContextMenu ? (event) => props.onContextMenu(eventParameters(event)) : null}
-          onMouseOver={(event) => {
-            props.onMouseOver && props.onMouseOver(eventParameters(event))
-            setInternalHover(true)
-          }}
-          onMouseOut={(event) => {
-            props.onMouseOut && props.onMouseOut(eventParameters(event))
-            setInternalHover(false)
-          }}
-        >
-          <path
-            d="M52 31.5C52 36.8395 49.18 42.314 45.0107 47.6094C40.8672 52.872 35.619 57.678 31.1763 61.6922C30.7916 62.0398 30.2084 62.0398 29.8237 61.6922C25.381 57.678 20.1328 52.872 15.9893 47.6094C11.82 42.314 9 36.8395 9 31.5C9 18.5709 18.6801 9 30.5 9C42.3199 9 52 18.5709 52 31.5Z"
-            fill={color}
-            stroke="white"
-            strokeWidth="4"
-          />
-          <circle cx="30.5" cy="30.5" r="8.5" fill="white" opacity={hover ? 0.98 : 0.6} />
-        </g>
-      </svg>
+      {props.children || (
+        <svg width={width} height={height} viewBox="0 0 61 71" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <g style={{ pointerEvents: 'auto' }}>
+            <path
+              d="M52 31.5C52 36.8395 49.18 42.314 45.0107 47.6094C40.8672 52.872 35.619 57.678 31.1763 61.6922C30.7916 62.0398 30.2084 62.0398 29.8237 61.6922C25.381 57.678 20.1328 52.872 15.9893 47.6094C11.82 42.314 9 36.8395 9 31.5C9 18.5709 18.6801 9 30.5 9C42.3199 9 52 18.5709 52 31.5Z"
+              fill={color}
+              stroke="white"
+              strokeWidth="4"
+            />
+            <circle cx="30.5" cy="30.5" r="8.5" fill="white" opacity={hover ? 0.98 : 0.6} />
+          </g>
+        </svg>
+      )}
     </div>
   )
 }
