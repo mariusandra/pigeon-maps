108d1f4faa1d21123c8b6ca3d20994d9756dc9e8
Luke Seelenbinder <lseelenbinder@users.noreply.github.com>
2022-10-25 13:00:06 +0200

    Add Stadia Maps as a provider.

--- src/providers.ts
+++ src/providers.ts
@@ -14,3 +14,7 @@ export function stamenTerrain(x: number, y: number, z: number, dpr = 1): string 
 export const maptiler = (apiKey: string, map = 'streets') => (x: number, y: number, z: number, dpr = 1): string => {
   return `https://api.maptiler.com/maps/${map}/256/${z}/${x}/${y}${dpr >= 2 ? '@2x' : ''}.png?key=${apiKey}`
 }
+
+export const stadiamaps = (style = 'alidade_smooth') => (x: number, y: number, z: number, dpr = 1): string => {
+  return `https://tiles.stadiamaps.com/styles/${style}/${z}/${x}/${y}${dpr >= 2 ? '@2x' : ''}.png
+}
